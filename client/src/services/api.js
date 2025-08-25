4d2e47278f8a2ce1bf4ac8a3b7f47876c157ccb3
Tomas Basualdo <tomaa.basualdo@gmail.com>
2025-08-25 20:55:49 -0300

    Deploy #2

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://backend-dietetica.onrender.com";
+const API_BASE_URL = "https://backend-dietetics.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
